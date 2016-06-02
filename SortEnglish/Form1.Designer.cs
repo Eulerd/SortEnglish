404f845a8a5a2c60ec85dd8f830e7986c7df3530
Eulerd <euler.volcano.dai@gmail.com>
2016-06-02 22:05:08 +0900

    英文並び替えプログラム

---
+++ SortEnglish/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace SortEnglish
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.開くToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開くToolStripMenuItem,
+            this.SaveToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(484, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 開くToolStripMenuItem
+            // 
+            this.開くToolStripMenuItem.Name = "開くToolStripMenuItem";
+            this.開くToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl+O";
+            this.開くToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.開くToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.開くToolStripMenuItem.Text = "開く";
+            this.開くToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(460, 412);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyDown);
+            // 
+            // SaveToolStripMenuItem
+            // 
+            this.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem";
+            this.SaveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.SaveToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.SaveToolStripMenuItem.Text = "保存";
+            this.SaveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 開くToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripMenuItem SaveToolStripMenuItem;
+
+
+    }
+}
+
---
+++ SortEnglish/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SortEnglish
+{
+    public partial class Form1 : Form
+    {
+        private List<string> InputSentence = new List<string>();
+        private string[][] words;
+        private string[] Ansers;
+        private string[] Questions;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string name,line;
+
+            FileDialog dialog = new OpenFileDialog
+            {
+                DefaultExt = "*.txt",
+                Filter = "テキストファイル|*.txt|すべてのファイル|*.*",
+                Title = "英文テキストを開く",
+            };
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                name = dialog.FileName;
+                StreamReader file = new StreamReader(name);
+                while ((line = file.ReadLine()) != null)
+                {
+                    InputSentence.Add(line);
+                }
+            }
+
+            int length = InputSentence.Count;
+
+            words = new string[length][];
+            Ansers = new string[length];
+            Questions = new string[length];
+            for (int i = 0; i < length; i++)
+            {
+                words[i] = InputSentence[i].Split(' ');
+                Ansers[i] += "A." + (i + 1).ToString() + " ";
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    words[i][j] = words[i][j].Replace('^', ' ');
+                    if (words[i][j].IndexOf('\'') >= 0) j++;
+                    Ansers[i] += (words[i][j] + " ");
+                }
+            }
+
+            Random r;
+            int num1, num2;
+            listBox1.Items.Add("question count : " + words.Length);
+            for (int i = 0; i < words.Length; i++)
+            {
+                int l = words[i].Length;
+                Swap(0, l - 2,i);
+                for (int j = 0; j < l; j++)
+                {
+                    r = new Random(j);
+                    num1 = r.Next(l);
+                    r = new Random(i + num1 % (l - j));
+
+                    do num2 = r.Next(l);
+                    while (num1 == num2);
+
+                    Swap(num1, num2, i);
+                }
+            }
+
+
+            for (int i = 0; i < length; i++)
+            {
+                Questions[i] += "Q." + (i + 1).ToString() + " (/ ";
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    Questions[i] += (words[i][j] + " / ");
+                    if (words[i][j].IndexOf('\'') >= 0) j++;
+                }
+                Questions[i] += ")";
+            }
+
+            listBox1.Items.Add("---ANSERS---");
+            listBox1.Items.AddRange(Ansers); //答えの文
+            listBox1.Items.Add("---QUESTIONS---");
+            listBox1.Items.AddRange(Questions); //問題の文
+        }
+
+        private void Swap(int num1, int num2,int i)
+        {
+            string word = words[i][num1];
+            words[i][num1] = words[i][num2];
+            words[i][num2] = word;
+        }
+
+
+
+        private void listBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Escape) this.Close();
+        }
+
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog()
+            {
+                DefaultExt = "*.txt",
+                Filter = "テキストファイル|*.txt|すべてのファイル|*.*",
+                FileName = "Problem",
+                Title = "英文並び替え問題を保存",
+            };
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter writer 
+                    = new StreamWriter(dialog.FileName, true, Encoding.GetEncoding("UTF-8"));
+                for (int i = 0; i < Questions.Length; i++)
+                    writer.WriteLine(Questions[i]);
+                writer.WriteLine();
+                for (int i = 0; i < Ansers.Length; i++)
+                    writer.WriteLine(Ansers[i]);
+                writer.Close();
+                MessageBox.Show("保存しました");
+            }
+        }
+    }
+}
